36930bf9b8fab268bb863c39277c209fa638747f
Jana Meißner <Jana.Meissner@Student.HTW-berlin.de>
2023-06-22 16:40:28 +0200

    FIX: removed shadows from transparent objects

--- src/RayTracer.java
+++ src/RayTracer.java
@@ -15,7 +15,7 @@ public class RayTracer {
 
     public RayTracer() {
         this.lights = new ArrayList<>();
-        this.objects = new ArrayList<>();
+        objects = new ArrayList<>();
         camera = new Camera(1920, 1080);
     }
 
@@ -32,26 +32,25 @@ public class RayTracer {
         RayTracer rayTracer = new RayTracer();
 
         // create quadrics
-        
         Quadric quadric2 = new Quadric(1f, -1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(0.4f, 0.8f, 0.3f), 0.8f, 0f, 0.2f, 0f, 0f)); //roughness+shinyness = 1
         Quadric sphereQuadricShadow = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(1f, 0f, 1f), 0.05f, 0f, 0.95f, 1.5f, 1f));
-        Quadric sphereBackGround = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(1f, 1f, 1f), 1f, 0f, 0f, 0f, 0f));
+        Quadric sphereBackGround = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(0.9f, 0.9f, 0.9f), 1f, 0f, 0f, 0f, 0f));
         Quadric sphereQuadric = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(0.8f, 0.1f, 0.5f), 0.5f, 0f, 0.5f, 1.5f, 0.3f));
         Quadric sphereQuadric2 = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(0f, 0f, 1f), 0.7f, 0f, 0.3f, 1.5f, 0.7f));
-        Quadric sphereQuadric3 = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(0.8f, 0.1f, 0.5f), 0.3f, 0f, 0.7f, 1.5f, 1f));
-        Quadric sphereQuadric4 = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(1, 0f, 0f), 0.2f, 0f, 0.8f, 0, 0f));
-        Quadric sphereQuadric5 = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(0.8f, 0.1f, 0.5f), 0.4f, 0f, 0.6f, 1.5f, 0.3f));
-        Quadric sphereQuadric6 = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(1,1,0f), 0.4f, 0f, 0.6f, 1.5f, 0.2f));
+        Quadric sphereQuadric3 = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(0.7f, 0.1f, 0.7f), 0.7f, 0f, 0.3f, 0f, 0f));
+        Quadric sphereQuadric4 = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(0.2f, 0.8f, 0.7f), 0.7f, 0f, 0.3f, 0, 0f));
+        Quadric sphereQuadric5 = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(0.8f, 0.1f, 0.5f), 0.7f, 0f, 0.3f, 0f, 0f));
+        Quadric sphereQuadric6 = new Quadric(1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, -1f, new Material(new Vector(0.8f, 0.1f, 0.5f), 0.8f, 0f, 0.2f, 0f, 0f));
 
         //transform quadrics
-        sphereQuadric = sphereQuadric.translate(new Vector(3, 0, -5)).scale(new Vector(1, 1, 1));
-        sphereQuadric2 = sphereQuadric2.translate(new Vector(2, 0, -5));
+        sphereQuadric = sphereQuadric.translate(new Vector(3, 0, -5.5f));
+        sphereQuadric2 = sphereQuadric2.translate(new Vector(2, 0, -5.5f));
         sphereQuadric3 = sphereQuadric3.translate(new Vector(-2, -1, -6.2f));
         sphereQuadric4 = sphereQuadric4.translate(new Vector(-3, -1, -6));
         sphereQuadric5 = sphereQuadric5.translate(new Vector(-3, 2, -6));
         sphereQuadric6 = sphereQuadric6.translate(new Vector(-3.5f, 2, -4));
         quadric2 = quadric2.translate(new Vector(1, 0, -8f));
-        sphereQuadricShadow = sphereQuadricShadow.translate(new Vector(2.5f, 1.5f, -3));
+        sphereQuadricShadow = sphereQuadricShadow.translate(new Vector(-2f, 0f, -6));
         sphereBackGround = sphereBackGround.scale(new Vector(50, 50, 50)).translate(new Vector(0, 0, -70));
 
         //CSG operations
@@ -60,13 +59,11 @@ public class RayTracer {
         CSG csgDifference = new CSG(sphereQuadric5, sphereQuadric6, "difference");
 
         // add objects
+        rayTracer.addObject(csgDifference);
         rayTracer.addObject(sphereQuadricShadow);
         rayTracer.addObject(quadric2);
-        rayTracer.addObject(sphereQuadric4);
-        rayTracer.addObject(sphereQuadric6);
-        //rayTracer.addObject(csgDifference);
-        /*rayTracer.addObject(csgIntersection);
-        rayTracer.addObject(csgUnion);*/
+        rayTracer.addObject(csgIntersection);
+        rayTracer.addObject(csgUnion);
         rayTracer.addObject(sphereBackGround);
 
         Light light1 = new Light(new Vector(2, 2, 5), 1f);
@@ -100,22 +97,19 @@ public class RayTracer {
 
     }
 
-    private Vector calculateRefractionDirection(Vector incidentDirection, Vector surfaceNormal, float refractionIndex) {
-
+    private Vector calculateRefractionDirection(Vector rayDirection, Vector surfaceNormal, float refractionIndex) {
         float i1 = 1.0f; // Brechungsindex des Mediums, aus dem der Strahl eintritt (normalerweise 1 für Vakuum oder Luft)
         float i2 = refractionIndex; // Brechungsindex des Mediums, in das der Strahl eintritt
         float i = i1 / i2;
 
-        if ( incidentDirection.multiply(-1).dotProduct(surfaceNormal) < 0){
+        if (rayDirection.multiply(-1).dotProduct(surfaceNormal) < 0){
             surfaceNormal = surfaceNormal.multiply(-1);
             i = i2/i1;
         }
-        float a = incidentDirection.multiply(-1).dotProduct(surfaceNormal);
-        float b = (float) Math.sqrt(1-i*i*(1-a*a));
-        Vector refractVector = incidentDirection.multiply(i).add(surfaceNormal.multiply(i*a-b));
-
+        float a = rayDirection.multiply(-1).dotProduct(surfaceNormal);
+        float b = (float) Math.sqrt(1-i*i*(1-a*a)); //negativ total reflection
+        Vector refractVector = rayDirection.multiply(i).add(surfaceNormal.multiply(i*a-b));
         return  refractVector;
-
     }
 
     private float calculateMicrofacetDistribution(float NdotH, float roughnessSquared) {
@@ -131,19 +125,17 @@ public class RayTracer {
     }
 
     public Vector getColor(Ray ray, int maxDepth) {
-
         if (maxDepth == 0) {
             return new Vector(0, 0, 0);
         }
 
         Intersection closestIntersection = new Intersection(null, Float.POSITIVE_INFINITY, null);
         Intersection tmp = null;
-        int idx=-1;
+        int idx = -1;
         for (int i = 0; i < objects.size(); i++) {
-
             Intersection intersection = objects.get(i).intersect(ray);
             if (intersection.intersection > 0 && intersection.intersection < closestIntersection.intersection) {
-                idx =i;
+                idx = i;
                 closestIntersection = intersection;
                 tmp = intersection;
                 // Vector intersectionVector = ray.getOrigin().add(ray.getDirection().normalize().multiply(closestIntersection)); // pos geaddet um kamera zu bewegen
@@ -161,17 +153,18 @@ public class RayTracer {
         boolean isInShadow = false;
         Ray shadowRay = new Ray(tmp.intersectionPoint.add(intersectionToLight.multiply(0.005f)), intersectionToLight);
         for (CSGObject object : objects) {
-            Intersection shadowIntersection = object.intersect(shadowRay);
-            if (shadowIntersection.intersection > 0 && shadowIntersection.intersection < intersectionToLight.length()) {
-                isInShadow = true;
-                break;
+            if (object.getMaterial(tmp).transparency < 0.5f) {
+                Intersection shadowIntersection = object.intersect(shadowRay);
+                if (shadowIntersection.intersection > 0 && shadowIntersection.intersection < intersectionToLight.length()) {
+                    isInShadow = true;
+                    break;
+                }
             }
         }
         // Calculate shadow factor
-        float shadowFactor = isInShadow ? 0.2f : 1.0f; // Adjust the shadow darkness here (e.g., 0.5f for 50% darkness)
+        float shadowFactor = isInShadow ? 0.3f : 1.0f; // Adjust the shadow darkness here (e.g., 0.5f for 50% darkness)
 
         // Calculate reflection color
-
         Vector reflectionColor = new Vector(0, 0, 0);
         Vector refractionColor = new Vector(0, 0, 0);
 
@@ -179,29 +172,14 @@ public class RayTracer {
             Vector reflectedDirection = ray.getDirection().subtract(normal.multiply(2 * ray.getDirection().dotProduct(normal)));
             Vector reflectionRayOrigin = tmp.intersectionPoint.add(reflectedDirection.multiply(0.001f)); // add small value to avoid hitting the same object
             Ray reflectedRay = new Ray(reflectionRayOrigin, reflectedDirection);
-            Vector reflectedColor = getColor( reflectedRay, maxDepth - 1);
-
-            reflectionColor = reflectedColor;
+            reflectionColor = getColor( reflectedRay, maxDepth - 1);
         }
         if (tmp.quadric.material.getTransparency() > 0) {
             Vector refractedDirection = calculateRefractionDirection(ray.getDirection(),normal,tmp.quadric.material.getTransmission());
             Vector refractedRayOrigin = tmp.intersectionPoint.add(refractedDirection.multiply(0.001f));
             Ray refractionRay = new Ray(refractedRayOrigin,refractedDirection);
-            Vector refractedColor =getColor(refractionRay, maxDepth-1);
-
-            reflectionColor =refractedColor;
+            reflectionColor = getColor(refractionRay, maxDepth-1);
         }
-
-
-
-        // Calculate refraction color
-        /*Vector refractedColor = new Vector(0, 0, 0);
-        if (material.counter == 1 && material.transparency > 0) {
-            Vector refractedDirection = calculateRefractionDirection(ray.getDirection(), normal, material.transmission);
-            Ray refractedRay = new Ray(intersection.intersectionPoint, refractedDirection);
-            material.counter = 0;
-            refractedColor = getColor(intersection, light, material, refractedRay, objects, maxDepth - 1);
-        }*/
 
         Vector viewDirection = ray.getOrigin().subtract(tmp.intersectionPoint).normalize();
         Vector lightDirection = intersectionToLight.normalize();
@@ -255,4 +233,3 @@ public class RayTracer {
         return finalColor.add(reflectionColor.multiply(tmp.quadric.material.getShinyness())).add(refractionColor.multiply(tmp.quadric.material.getTransparency()));  // add(refractedColor.multiply(material.transparency)) auskommentiert
     }
 }
-
